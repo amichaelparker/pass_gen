f25f85939fba28668fece40c2e4f2ed8827ce8aa
Adam Parker <amichaelparker@gmail.com>
2017-01-20 17:45:52 -0600

    added initial argparse functionality

--- passgen.py
+++ passgen.py
@@ -1,11 +1,22 @@
 import random
+import sys
+import argparse
 
-def main():
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Return a password.')
+    parser.add_argument('n', type=int, help='Number of words to generate')
+    parser.parse_args(sys.argv[1:])
+
+def pass_gen(length):
     password = ''
-    for word in range(4):
+    for word in range(length):
         password += random.choice(open('words.txt').readlines()).rstrip()
 
-    print(password)
+    return password
+
+def main():
+    parse_arguments()
+    print(pass_gen(int(sys.argv[1])))
 
 if __name__ == "__main__":
     main()
