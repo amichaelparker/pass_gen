0f63c351eb7060cc791b8d639f62752de30e4da3
Adam Parker <amichaelparker@gmail.com>
2017-01-20 19:10:37 -0600

    add argument support for capitalized words

--- passgen.py
+++ passgen.py
@@ -2,21 +2,44 @@ import random
 import sys
 import argparse
 
+def caps_test(word):
+    if word[0] == word[0].upper():
+        return True
+
+    return False
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Return a password.')
-    parser.add_argument('n', type=int, help='Number of words to generate')
-    parser.parse_args(sys.argv[1:])
+    parser.add_argument('n', type=int, nargs='?', help='number of words to generate', default=4)
+    parser.add_argument('-C', action='store_const', const=True, help='allow capitalized words')
 
-def pass_gen(length):
+    argument = parser.parse_args(sys.argv[1:])
+    test = vars(argument)
+    print(test)
+
+    return test
+
+def pass_gen(length, caps):
     password = ''
-    for word in range(length):
-        password += random.choice(open('words.txt').readlines()).rstrip()
+
+    for _ in range(length):
+        new_word = ''
+
+        if caps:
+            new_word = random.choice(open('words.txt').readlines()).rstrip()
+        else:
+            new_word = random.choice(open('words.txt').readlines()).rstrip()
+            while caps_test(new_word):
+                new_word = random.choice(open('words.txt').readlines()).rstrip()
+
+        password += new_word
 
     return password
 
 def main():
-    parse_arguments()
-    print(pass_gen(int(sys.argv[1])))
+    arguments = parse_arguments()
+    print(pass_gen(arguments['n'], arguments['C']))
+
 
 if __name__ == "__main__":
     main()
